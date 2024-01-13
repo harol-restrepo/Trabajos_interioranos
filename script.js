55b14a4c70ed762c84ece94c1a0e74523e95400c
harol-restrepo <156538422+harol-restrepo@users.noreply.github.com>
2024-01-13 13:51:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function mostrarConsejos() {
-    alert("Aquí van los consejos de estudio.");
+function mostrarCursos() {
+    alert("Explora nuestros emocionantes cursos en línea para mejorar tus habilidades académicas.");
 }
 
 function mostrarRecursos() {
-    alert("Aquí van los recursos en línea.");
+    alert("Descubre una variedad de recursos educativos, desde libros hasta videos, para apoyar tu aprendizaje.");
 }
 
 function mostrarAsesoramiento() {
-    alert("Aquí van los recursos de asesoramiento psicológico.");
+    alert("Nuestro equipo de asesoramiento académico está aquí para brindarte consejos y orientación.");
+}
+
+function mostrarFormulario() {
+    var trabajoInfo = prompt("Ingrese la información sobre el trabajo que desea mejorar a nota:");
+    if (trabajoInfo !== null && trabajoInfo.trim() !== "") {
+        alert("Gracias por enviar la información. Trabajaremos para proporcionar recursos que te ayuden.");
+    } else {
+        alert("No se ingresó información. Por favor, inténtelo de nuevo si desea enviar detalles sobre su trabajo.");
+    }
 }
