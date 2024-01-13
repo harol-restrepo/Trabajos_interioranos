6ec003df7aff5d9c0982d71ac1f0d5063167d30b
harol-restrepo <156538422+harol-restrepo@users.noreply.github.com>
2024-01-13 13:05:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarConsejos() {
+    alert("Aquí van los consejos de estudio.");
+}
+
+function mostrarRecursos() {
+    alert("Aquí van los recursos en línea.");
+}
+
+function mostrarAsesoramiento() {
+    alert("Aquí van los recursos de asesoramiento psicológico.");
+}
